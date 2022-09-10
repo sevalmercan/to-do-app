ceb20bd2783e31ced24096a5e01567c7cb0a6dce
sevalmercan <sevalmercan.sm@gmail.com>
2022-09-10 22:57:10 +0300

    vue router added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <to-do-app />
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/register">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import toDoApp from "./to-do-app.vue";
-export default {
-  name: "App",
-  components: { toDoApp },
-};
-</script>
-
 <style>
-.body {
-  margin: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/login-view.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <div>   
+  <form @submit.prevent="login" class="login">     
+    <h2>Login</h2>     
+    <input       
+      type="email"       
+      placeholder="Email address..."       
+      v-model="email"     
+    />     
+    <input       
+      type="password"       
+      placeholder="password..."       
+      v-model="password"     
+    />     
+    <button type="submit">
+       Login
+    </button>   
+  </form> 
+</div>  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      email:"",
+      password:""
+    }
+  }
+
+}
+</script>
+
+<style>
+  .login{
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    margin: 0 auto;
+  }
+</style>
